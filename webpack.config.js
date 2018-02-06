1b30ce113933e319cde3e44902426c9e168db41c
Maciej Treder <contact@maciejtreder.com>
2018-02-06 14:17:10 +0100

    Angular & Webpack demystified - second step

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,35 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = function () {
-   return {
-       entry: './src/main.ts',
-       output: {
-           path: __dirname + '/dist',
-           filename: 'app.js'
-       },
+    return {
+        entry: './src/main.ts',
+        output: {
+            path: __dirname + '/dist',
+            filename: 'app.js'
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
 
-       plugins: [
-           new HtmlWebpackPlugin({
-               template: __dirname + '/src/index.html',
-               output: __dirname + '/dist',
-               inject: 'head'
-           }),
-           new CopyWebpackPlugin([
-               { from: 'src/assets', to: 'assets'}
-           ])
-       ]
-   };
+        module: {
+            rules: [
+                {test: /\.ts$/, loader: 'awesome-typescript-loader'}
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + '/src/index.html',
+                output: __dirname + '/dist',
+                inject: 'head'
+            }),
+            new CopyWebpackPlugin([
+                { from: 'src/assets', to: 'assets'}
+            ]),
+            new ScriptExtPlugin({
+                defaultAttribute: 'defer'
+            })
+        ]
+    };
 }
-
